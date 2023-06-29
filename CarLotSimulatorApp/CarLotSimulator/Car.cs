754fdddab13c89cb741d00f8cd197c3cef27ded0
Isaiah Wilson <isaiahrwilson@gmail.com>
2023-06-29 14:57:35 -0500

    CarLotSimulatorApp

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,96 @@
+﻿using System;
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public string EngineNoise { get; set; }
+
+        public string HonkNoise { get; set; }
+
+        public bool IsDriveable { get; set; }
+
+
+
+        public void MakeEngineNoise()
+        {
+            Console.WriteLine($" The engine is running {EngineNoise} .");
+        }
+
+        public void MakeHonkNoise()
+        {
+            Console.WriteLine($"The car is honking {HonkNoise}.");
+        }
+
+
+
+
+        public class myCar2
+        {
+            public string Make { get; set; }
+
+            public string Model { get; set; }
+
+            public int Year { get; set; }
+
+            public string EngineNoise2 { get; set; }
+
+            public string HonkNoise2 { get; set; }
+
+            public bool IsDriveable { get; set; }
+
+
+
+            public void MakeEngineNoise()
+            {
+                Console.WriteLine($"The engine is loud {EngineNoise2}.");
+            }
+
+            public void MakeHonkNoise()
+            {
+                Console.WriteLine($"The car is honking at me {HonkNoise2}.");
+            }
+
+
+
+
+            public class myCar3
+            {
+                public string Make { get; set; }
+
+                public string Model { get; set; }
+
+                public int Year { get; set; }
+
+                public string EngineNoise3 { get; set; }
+
+                public string HonkNoise3 { get; set; }
+
+                public bool IsDriveable { get; set; }
+
+
+
+                public void MakeEngineNoise()
+                {
+                    Console.WriteLine($"The engine is loud {EngineNoise3}.");
+                }
+
+                public void MakeHonkNoise()
+                {
+                    Console.WriteLine($"The car is honking at me {HonkNoise3}.");
+                }
+
+
+
+
+
+
+            }
+        }
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -8,15 +8,72 @@ namespace CarLotSimulator
         {
             //TODO
 
-            //Create a seperate class file called Car
-            //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
-            //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
-            //The methods should take one string parameter: the respective noise property
+            //Create a seperate class file called Car- done
+            //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable- done
+            //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()- done
+            //The methods should take one string parameter: the respective noise property -done
 
 
             //Now that the Car class is created we can instanciate 3 new cars
-            //Set the properties for each of the cars
-            //Call each of the methods for each car
+
+            //created car 1
+            Car myCar = new Car();
+
+            //set the property values using dot notation
+            myCar.Make = "Dodge";
+            myCar.Model = "Charger";
+            myCar.Year = 2022;       
+            myCar.IsDriveable= true;
+            myCar.EngineNoise = "vroom vroom";
+            myCar.HonkNoise = "honkkk!";
+
+            //called methods
+            myCar.MakeEngineNoise();
+            myCar.MakeHonkNoise();
+
+
+
+            //created car 2
+            Car myCar2 = new Car();
+
+            //set the property values using dot notation
+            myCar2.Make = "Mclaren";
+            myCar2.Model = "720s";
+            myCar2.Year = 2021;
+            myCar2.IsDriveable = true;
+            myCar2.EngineNoise = "vromm shpoosh";
+            myCar2.HonkNoise = "honkkk honk honkkkk";
+
+            //called methods
+            myCar2.MakeEngineNoise();
+            myCar2.MakeHonkNoise();
+
+
+
+
+            //created car 3
+            Car myCar3 = new Car();
+
+            //set the property values using dot notation
+            myCar3.Make = "Jeep";
+            myCar3.Model = "Trackhawk";
+            myCar3.Year = 2020;
+            myCar3.IsDriveable = true;
+            myCar3.EngineNoise = "vromm popppp popp";
+            myCar3.HonkNoise = "honkkk honkkkk";
+
+            //called methods
+            myCar3.MakeEngineNoise();
+            myCar3.MakeHonkNoise();
+
+
+
+
+
+
+
+            //Set the properties for each of the cars -done
+            //Call each of the methods for each car - done
 
             //*************BONUS*************//
 
